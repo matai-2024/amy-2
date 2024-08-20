9d39585609e33c27ff6a0f3bac4129f2b6d13133
achoo-o <amyj.etana@gmail.com>
2024-08-21 08:42:48 +1200

    Style using SASS with basic intro content

--- client/components/home/Intro.tsx
+++ client/components/home/Intro.tsx
@@ -3,6 +3,8 @@ export default function Intro() {
     <>
       <h1>Amy Jackson</h1>
       <h2>Front-end Software Developer</h2>
+      <p>I enjoy diverse and continuous learning opportunities.</p>
+      <p>Currently doodling and creating engaging projects from Auckland, New Zealand.</p>
     </>
   )
 }
