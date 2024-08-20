ac7f40f531f882558b8fbde9edb5061274471768
achoo-o <amyj.etana@gmail.com>
2024-08-21 07:13:16 +1200

    Add basic component template

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,11 @@
+import Main from "./Main"
+import Footer from "./Footer"
+
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <Main />
+      <Footer />
     </div>
   )
 }
